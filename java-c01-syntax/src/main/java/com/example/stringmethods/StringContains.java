135cb4cdb4d55a9c5fba076f30d495d81d9dfa1f
alansastre <alan@alansastre.co>
2023-01-10 13:36:06 +0100

    syntax - operators

---
+++ java-c01-syntax/src/main/java/com/example/stringmethods/StringContains.java
@@ -0,0 +1,12 @@
+package com.example.stringmethods;
+
+public class StringContains {
+
+    public static void main(String[] args) {
+
+        String text = "hola mundo y adios";
+
+        boolean contains = text.contains("hola");
+        System.out.println(contains);
+    }
+}
