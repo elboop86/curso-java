fea95f7a7f9ccd68548171ffd409e9422f3da96a
alansastre <alan@alansastre.co>
2023-01-11 09:43:07 +0100

    operadores - lógicos

--- java-c01-syntax/src/main/java/com/example/operators/Logic.java
+++ java-c01-syntax/src/main/java/com/example/operators/Logic.java
@@ -4,6 +4,9 @@ public class Logic {
 
     public static void main(String[] args) {
 
+        /*
+        Operadores lógicos: and y or
+         */
         int number1 = 10;
         int number2 = 10;
 
@@ -13,5 +16,19 @@ public class Logic {
         boolean result2 = number1 > 5 && number1 < 100 && number2 > 1 && number2 < 100;
 
         // || (OR): comprueba que AL MENOS UNA sea verdadera
+        boolean result3 = 5 < 10 || 20 < 15; // true || false
+
+        // Combinar && (AND) y || (OR)
+        boolean result4 = 5 < 5 || 50 <= 100 && 10 < 10;
+        // false || true && false
+        // true && false
+        // false
+        System.out.println(result4);
+
+        boolean result5 = 5 < 5 || (50 <= 100 && 10 < 10);
+        // false || true && false
+        // true && false
+        // false
+        System.out.println(result5);
     }
 }
