3cbd61519989a2ddd9c991fb9ef99f6d9e7b44f3
alansastre <alan@alansastre.co>
2023-01-11 13:48:58 +0100

    estructuras de control - for

---
+++ java-c01-syntax/src/main/java/com/example/control/repetitive/For.java
@@ -0,0 +1,19 @@
+package com.example.control.repetitive;
+
+public class For {
+
+    public static void main(String[] args) {
+
+        /*
+        Estructuras de control repetitivo permiten repetir un código
+
+        for(inicio; fin; incremento)
+         */
+
+        for(int i = 0; i < 1_000_000; i++)
+            System.out.println("Número: " + i);
+
+
+
+    }
+}
