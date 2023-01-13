8fa78d53f8c1da98f42b5edba6ed22809b31c937
alansastre <alan@alansastre.co>
2023-01-13 11:16:52 +0100

    oop - clase Vehicle métodos comportamiento

--- java-c02-oop/src/main/java/com/example/classvehicle/Main.java
+++ java-c02-oop/src/main/java/com/example/classvehicle/Main.java
@@ -7,9 +7,25 @@ public class Main {
         System.out.println("Velocidad máxima: " + Vehicle.MAX_SPEED);
 
         Vehicle fordFiesta = new Vehicle("Ford", "Fiesta", 1.2, "SEDAN", 0, false);
+        System.out.println(fordFiesta.manufacturer);
 
         Vehicle alfaRomeo = new Vehicle("Alfa Romeo", "149", 1.9, "SEDAN");
 
+        // los métodos cambian el estado del vehículo es decir,
+        // los valores de sus atributos
+        alfaRomeo.turnOn();
 
+        alfaRomeo.speedUp(50);
+
+        alfaRomeo.speedUp(100);
+
+        alfaRomeo.speedDown(40);
+
+        alfaRomeo.speedDown(40);
+
+        alfaRomeo.turnOff();
+
+        // TODO imprimir modelo completo
+        // TODO agregar precio
     }
 }
--- java-c02-oop/src/main/java/com/example/classvehicle/Vehicle.java
+++ java-c02-oop/src/main/java/com/example/classvehicle/Vehicle.java
@@ -12,6 +12,7 @@ public class Vehicle {
 
     // 1. constantes
     static final int MAX_SPEED = 120;
+    static final int MIN_SPEED = 0;
 
     // 2. atributos/propiedades
     String manufacturer;
@@ -43,12 +44,27 @@ public class Vehicle {
         // this.status = false;
     }
 
-    // 4. métodos
+    // 4. métodos (comportamiento)
     public void speedUp(int quantity) {
-         this.speed += quantity;
+        // si sumar quantity a speed supera 120 entonces se pone la velocidad en 120
+        // si no, entonces se suma la cantidad
+        if(this.speed + quantity > MAX_SPEED) // 120 + 50 = 170. No subir la velocidad
+            this.speed = MAX_SPEED; // 120 límite superior
+        else
+            this.speed += quantity;
     }
 
     public void speedDown(int quantity) {
-        this.speed -= quantity;
+        if(this.speed - quantity < MIN_SPEED)
+            this.speed = MIN_SPEED; // 0 límite inferior
+        else
+            this.speed -= quantity;
+
+    }
+    public void turnOn(){
+        this.status = true;
+    }
+    public void turnOff(){
+        this.status = false;
     }
 }
